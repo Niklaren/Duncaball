d6fd2d560d6e141de2bc5992195d93526c668c6e
Duncan Gillespie <intoxicatedkoala@gmail.com>
2015-05-16 14:39:53 +0100

    0.03 Bounce

---
+++ Assets/Standard Assets/Scripts/bounce.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bounce : MonoBehaviour {
+	Rigidbody r;
+	public float strength;
+	
+	void OnCollisionEnter (Collision col)
+	{
+	print ("col");
+		//r = col.gameObject.GetComponent<Rigidbody>();
+		//r.AddForce(transform.up * strength);
+		col.gameObject.GetComponent<Rigidbody>().AddForce(transform.up * strength);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
