b3bb3aaaf81c6e7781af912c2d7ed71a6ce23013
Nick <pitfall@hotmail.co.uk>
2015-05-16 17:36:11 +0100

    0.05
    some movement stuff

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,9 +5,18 @@ public class Player : MonoBehaviour {
 
 	public float pitch, yaw, roll;
 
+	public float jump_speed = 6.6f;
+	public float run_speed = 0.5f;
+	private float back_speed;
+	private float side_speed;
+
+	public Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+		back_speed = 0.25f * run_speed;
+		side_speed = 0.75f * run_speed;
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -25,24 +34,16 @@ public class Player : MonoBehaviour {
 		float deltaX = Input.GetAxis("Mouse X");
 		float deltaY = Input.GetAxis ("Mouse Y");
 		pitch += deltaX;
-		yaw += deltaY;
+		yaw -= deltaY;
 		roll += 0;
 
 		WrapAngle(pitch);
 		WrapAngle(yaw);
 		WrapAngle(roll);
 
-		transform.localEulerAngles = new Vector3(0, pitch, yaw);
+		transform.localEulerAngles = new Vector3(yaw, pitch, 0);
 		//transform.rotation.x = (transform.rotation.x + deltaX);
 		//transform.localRotation = Quaternion.Euler (pitch, yaw, roll);
-	}
-
-	public static void WrapAngle(float angle)
-	{
-		if (angle < -360F)
-			angle += 360F;
-		if (angle > 360F)
-			angle -= 360F;
 	}
 
 	void Move(){
@@ -51,7 +52,14 @@ public class Player : MonoBehaviour {
 	}
 	
 	void ControlledMovement(){
-		
+		if (Input.GetKey ("w"))
+			MoveForwards (run_speed);
+		if (Input.GetKey ("s"))
+			MoveForwards (-back_speed);
+		if (Input.GetKey ("a"))
+			MoveLeft (side_speed);
+		if (Input.GetKey ("d"))
+			MoveRight (side_speed);
 	}
 	
 	void UncontrolledMovement(){
@@ -60,6 +68,9 @@ public class Player : MonoBehaviour {
 	
 	void Jump(){
 		Debug.Log ("jump");
+		Vector3 jump_strength = (transform.up * jump_speed);
+		Debug.Log (jump_strength);
+		rb.AddForce(jump_strength);
 	}
 	
 	void Slide(){
@@ -72,5 +83,34 @@ public class Player : MonoBehaviour {
 	
 	void Tackle(){
 		
+	}
+
+
+	public static void WrapAngle(float angle)
+	{
+		if (angle < -360F)
+			angle += 360F;
+		if (angle > 360F)
+			angle -= 360F;
+	}
+
+	void MoveForwards(float f)
+	{
+		transform.position += (f * transform.forward);
+	}
+
+	void MoveLeft(float f)
+	{
+		transform.position -= (f * transform.right);
+	}
+
+	void MoveRight(float f)
+	{
+		transform.position += (f * transform.right);
+	}
+
+	void Strafe(float s)
+	{
+		transform.position += (s * transform.right);
 	}
 }
