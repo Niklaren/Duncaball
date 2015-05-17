9cbd6153585ccb5d38cf547ee72b282773d26d25
pitfall <pitfall@hotmail.co.uk>
2015-05-17 20:59:57 +0100

    0.09
    pickup & throw

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	private Rigidbody rb;
+
+	enum Team{A, B};
+	Team team;
+
+	public GameObject ball;
+	Vector3 ballOffset;
+
 	public float pitch, yaw, roll;
-	public Camera camera;
+	//public Camera camera;
 
 	public float jump_speed = 6.6f;
 	public float run_speed = 0.5f;
@@ -17,26 +25,45 @@ public class Player : MonoBehaviour {
 	private float distance_to_ground;
 	private Vector3 velocity;
 
-	private Rigidbody rb;
+	public GameObject grab_zone;
+	bool has_ball;
 
 	// Use this for initialization
 	void Start () {
+		Screen.lockCursor = true;
+		//Cursor.lockState = true;
+		ballOffset = new Vector3(0.0f, 0.0f, 1.0f);
+
 	    ground_threshhold = 0.1f;
 		back_speed = 0.25f * run_speed;
 		side_speed = 0.75f * run_speed;
 		rb = GetComponent<Rigidbody>();
 		distance_to_ground = GetComponent<Collider>().bounds.extents.y + ground_threshhold;
 
+		foreach (Transform child in transform){
+			if (child.name == "grab zone"){
+				grab_zone = child.gameObject;
+			}
+		}
+		ball = GameObject.FindWithTag ("ball");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Move ();
 
-		print(rb.velocity.magnitude);
+		//print(rb.velocity.magnitude);
 
 		Look ();
-		
+
+		if(Input.GetKeyDown ("e"))
+			GrabBall ();
+		if (has_ball)
+			CarryBall ();
+		if (Input.GetMouseButtonDown (0))
+			ThrowBall ();
+		if(Input.GetMouseButtonDown (1))
+			CarryBall ();
 	}
 
 	void CheckGrounded()
@@ -95,16 +122,47 @@ public class Player : MonoBehaviour {
 	void Jump(){
 		Debug.Log ("jump");
 		Vector3 jump_strength = (transform.up * jump_speed);
-		Debug.Log (jump_strength);
+		//Debug.Log (jump_strength);
 		rb.AddForce(jump_strength);
 	}
 	
 	void Slide(){
 		
 	}
-	
+
+	void GrabBall(){
+		Debug.Log ("grab try");
+		Collider[] colliders_in_zone = Physics.OverlapSphere (transform.position, 1.5f);
+		int i = 0;
+		while (i < colliders_in_zone.Length) {
+			if (colliders_in_zone[i].tag == "ball"){
+				Debug.Log("grab success");
+				PickupBall();
+			}
+			i++;
+		}
+	}
+
+	void PickupBall(){
+		has_ball = true;
+		ball.GetComponent<Rigidbody> ().angularVelocity.Equals (new Vector3 (0, 0, 0));
+		//ball.GetComponent<Transform>().
+	}
+
+	void CarryBall(){
+		Debug.Log ("carry");
+		ball.transform.position = transform.position + ballOffset;
+	}
+
 	void ThrowBall(){
-		
+		Debug.Log ("throw try");
+		if (has_ball) {
+			has_ball = false;
+			Debug.Log("throw success");
+			Vector3 throw_direction = grab_zone.GetComponent<Transform> ().forward;
+			float throw_force = 100.0f;
+			ball.GetComponent<Rigidbody> ().AddForce (throw_direction * throw_force);
+		}
 	}
 	
 	void Tackle(){
